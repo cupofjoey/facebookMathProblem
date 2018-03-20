eb13c97c2a939d879557869635d9184a84753496
Joe Hill <joe@jkh3.com>
2018-03-19 22:47:44 -0400

    Added function that would be necessary for the answer to be 1

--- mathProblem.playground/Contents.swift
+++ mathProblem.playground/Contents.swift
@@ -7,3 +7,8 @@ func math(x:Int, y:Int, z:Int ) -> Int {
     return(x / y*(z + y))
 }
 math(x:6, y:2, z:1)
+
+func math2(x:Int, y:Int, z:Int ) -> Int {
+    return(x / (y*(z + y)))
+}
+math2(x:6, y:2, z:1)
